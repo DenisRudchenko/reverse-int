254831fec31ba591790a30dd01ccea1c95c5990f
DenisRudchenko <xpyctemo@gmail.com>
2023-01-15 20:33:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
-}
+    n = Math.abs(n) + "";
+    let breakStr = n.split("");
+    let res = breakStr.reverse();
+    let result = res.join('');
+    return result;
+};
